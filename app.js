8526386e8ffca5f5412c29eeb5d7fe3cf4f8d5f4
HukovaNastya <hukovanasta@gmail.com>
2024-06-18 21:56:41 +0300

    added_solution_for_second_task

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+//first task
+
 const proPlan = document.getElementById("pro-plan");
 const cardBody = proPlan.querySelector(".card-body");
 const proPlanList = cardBody.querySelector(".list-unstyled");
 
 proPlanList.insertAdjacentHTML("beforeend", ` <li>24/7 Phone support</li>`);
 
-console.log("prpOlan", proPlanList);
+//second task
+const container = document.querySelector(".card-deck");
+container.classList.add("d-flex", "justify-content-flex-end", "flex-direction-reverse");
+
