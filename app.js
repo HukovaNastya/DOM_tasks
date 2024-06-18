c70a023ee59059fd24254ccfbe3b614319669c0b
HukovaNastya <hukovanasta@gmail.com>
2024-06-18 22:02:29 +0300

    added_solution_for_third_task

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 //first task
 
 const proPlan = document.getElementById("pro-plan");
-const cardBody = proPlan.querySelector(".card-body");
-const proPlanList = cardBody.querySelector(".list-unstyled");
+const cardBodyPrpPlan = proPlan.querySelector(".card-body");
+const proPlanList =  cardBodyPrpPlan.querySelector(".list-unstyled");
 
 proPlanList.insertAdjacentHTML("beforeend", ` <li>24/7 Phone support</li>`);
 
 //second task
 const container = document.querySelector(".card-deck");
 container.classList.add("d-flex", "justify-content-flex-end", "flex-direction-reverse");
+
+//third task
+const getStartedBtn = cardBodyPrpPlan.querySelector(".btn");
+getStartedBtn .classList.add("background-blue", "color-white");
+getStartedBtn.innerText = "Buy Now";
 
