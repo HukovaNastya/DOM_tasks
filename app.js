12b24255487a12c63e21180bea2b2cd7cfc98ede
HukovaNastya <hukovanasta@gmail.com>
2024-07-06 23:28:07 +0300

    added_forth_task

--- app.js
+++ app.js
@@ -8,10 +8,46 @@ proPlanList.insertAdjacentHTML("beforeend", ` <li>24/7 Phone support</li>`);
 
 //second task
 const container = document.querySelector(".card-deck");
-container.classList.add("d-flex", "justify-content-flex-end", "flex-direction-reverse");
+container.classList.add("d-flex", "justify-content-flex-end", "flex-direction-reverse", "align-items-center");
 
 //third task
 const getStartedBtn = cardBodyPrpPlan.querySelector(".btn");
 getStartedBtn .classList.add("background-blue", "color-white");
 getStartedBtn.innerText = "Buy Now";
+
+//fourth tasks
+container.style.padding = "10px 30px 0  30px";
+
+const toggleBtnContainer = document.createElement('div');
+toggleBtnContainer.classList.add('toggle-btn', );
+
+toggleBtnContainer.insertAdjacentHTML("afterbegin", `   
+<label class="toggle">
+  <span class="year-month">Monthly</span>
+  <input type="checkbox" />
+  <span class="slider round"></span>
+</label>
+`)
+
+container.insertBefore(toggleBtnContainer, container.childNodes[2]);
+
+const toggle = document.querySelector('.toggle input')
+const basicPlan = document.getElementById("basic-plan");
+const proPlanPyament = proPlan.querySelector(".pricing");
+const basicPlanPyament = basicPlan.querySelector(".pricing");
+
+const applySaleForYearMode = (price) => {
+    return  price = `${parseFloat(price) * 10} / year`;
+}
+
+toggle.addEventListener('click', () => {
+    const yearMonth = toggle.parentNode.querySelector('.year-month')
+    yearMonth.textContent = toggle.checked ? 'Yearly' : 'Monthly';
+
+
+    if(yearMonth.textContent === "Yearly") {
+        proPlanPyament.textContent = applySaleForYearMode(proPlanPyament.textContent);
+        basicPlanPyament.textContent = applySaleForYearMode(basicPlanPyament.textContent);
+    }
+})
 
